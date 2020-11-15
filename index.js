9acba5e17cd5ffe2f3275935ab7629de3b40fa0a
John Hayes <johnhayesru@gmail.com>
2020-11-15 05:15:10 -0600

    feat(server): read/decode/write stream containing payload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // Import dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // Respond to requests with a string
 const server = http.createServer((req, res) => {
@@ -25,20 +26,35 @@ const server = http.createServer((req, res) => {
   // Set headers variable as an object
   const headers = req.headers;
 
-  // Send server response
-  res.end("Hello World!\n");
+  // Set payload to variable, if any
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
 
-  // Log headers
-  console.log(
-    `Request received with these headers: ${JSON.stringify(headers)}`
-  );
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
 
-  // Log path, method, and queries
-  console.log(
-    `Request received on path ${trimmedPath} with method ${method} and these query parameters ${JSON.stringify(
-      queryStringObject
-    )}`
-  );
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    // Send server response
+    res.end("Hello World!\n");
+
+    // Log headers
+    console.log(
+      `Request received with these headers: ${JSON.stringify(headers)}`
+    );
+
+    // Log path, method, and queries
+    console.log(
+      `Request received on path ${trimmedPath} with method ${method} and these query parameters ${JSON.stringify(
+        queryStringObject
+      )}`
+    );
+
+    // Log payload
+    console.log(`Request received with this payload: ${buffer}`);
+  });
 });
 
 // Initialize server
