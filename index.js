591cc5d117583abb6927e10d644b028566974797
John Hayes <johnhayesru@gmail.com>
2020-11-15 04:25:33 -0600

    feat(server): parse incoming URL paths and add to variable

--- index.js
+++ index.js
@@ -5,10 +5,21 @@
 
 // Import dependencies
 const http = require("http");
+const url = require("url");
 
 // Respond to requests with a string
 const server = http.createServer((req, res) => {
-  res.end("Hello World!");
+  // Parse incoming URL
+  const parseUrl = url.parse(req.url, true);
+
+  // Set path variable from URL
+  const path = parseUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send server response
+  res.end("Hello World!\n");
+
+  console.log(`Request received on path: ${trimmedPath}`);
 });
 
 // Initialize server
